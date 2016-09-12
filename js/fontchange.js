29385c1539e14c8cf60f7bdaac101c7eb00c856d
vivian-yawei <15800957204@163.com>
2016-09-12 19:16:35 +0800

    self

--- js/fontchange.js
+++
@@ -1,8 +0,0 @@
-// JavaScript Document
-(function(win,doc){
-	function setFont(){
-		doc.documentElement.style.fontSize=doc.documentElement.clientWidth/320*20+'px';
-	}
-	setFont();
-	win.addEventListener('resize',setFont,false);//屏幕缩小放大都可以使用
-})(window,document);
